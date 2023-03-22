7f35325618756b7727ce5f5235d688bc5f508fa0
s0xfan <s0xfan@comcast.net>
2023-03-22 14:48:58 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023CTHAR",
+      "defaultValue": "2023cthar",
       "required": "true",
       "disabled": "true"
     },
